d6ee63f08290c5e8a75168631dc0b6cd0407f4c9
Promise Morka <onyekamorka06@gmail.com>
2021-07-03 03:57:24 +0000

    Added reverse method

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ console.log(arr.slice(-1));
 console.log(arr.slice(1, 4));
 console.log(arr.slice());
 
-// SPLICE METHOD - This is the same as the slice method only that it changes(mutates)
+// SPLICE METHOD - This is the same as the slice method only that it changes(mutates) the arr
 console.log(arr.splice(2));
 console.log(arr);
+
+// REVERSE - as the name states this method reverses the elements on an array and just like the splice method, it mutates the original array
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+
