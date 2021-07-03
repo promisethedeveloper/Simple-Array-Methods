b361d46de42e7c417ff6e0abacf909d667f526aa
Promise Morka <onyekamorka06@gmail.com>
2021-07-03 04:05:07 +0000

    Added concat method

--- script.js
+++ script.js
@@ -16,4 +16,7 @@ const arr2 = ['j', 'i', 'h', 'g', 'f'];
 console.log(arr2.reverse());
 console.log(arr2);
 
-
+// CONCAT - This is used to join two arrays, and it does not change the original array
+let arr1 = ['a', 'b', 'c', 'd', 'e'];
+const alphabets = arr1.concat(arr2);
+console.log(alphabets);
